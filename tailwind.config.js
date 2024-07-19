3ac1d843ce0e1987bc0ce835987ff20052885134
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-07-18 21:34:18 -0300

    estilizando card

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        //figtree: ["Figtree",  sans-serif],
       },
     },
   },
